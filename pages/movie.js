7fa66528d8a7418cb9188e69b81a0f39c26b31b6
Gerardo Garza <owlscloud@gmail.com>
2021-05-26 22:38:49 -0500

    netlify

--- pages/movie.js
+++ pages/movie.js
@@ -1,5 +1,4 @@
 import Head from "next/head";
-import Image from "next/image";
 import MainLayout from "../components/layouts/MainLayout";
 import FeaturedMedia from "../components/UI/FeaturedMedia/FeaturedMedia";
 import PosterView from "../components/UI/PosterView/PosterView";
