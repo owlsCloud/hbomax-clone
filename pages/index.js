e7281778f84290ab22221c06e77ecc523b8f1169
Gerardo Garza <owlscloud@gmail.com>
2021-05-27 09:55:47 -0500

    login component

--- pages/index.js
+++ pages/index.js
@@ -1,29 +1,16 @@
 import Head from "next/head";
-import { useStateContext } from "../components/HBOProvider";
+import Login from "../components/UI/Login/Login";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
 export default function Home() {
-  const globalState = useStateContext();
+  const router = useRouter();
+  useEffect(() => {
+    const loggedIn = false;
+    if (!loggedIn) router.push("/create");
+  }, []);
   return (
     <div>
-      <div className="login-user">
-        <div className="login-user__top">
-          <div className="login-user__logo" />
-          <span className="login-user__title">Who Is Watching?</span>
-        </div>
-        <div className="login-user__form">
-          <div className="login-user__user-box">
-            <img
-              className="login-user__user-img"
-              src="https://images-na.ssl-images-amazon.com/images/M/MV5BMjEzMjA0ODk1OF5BMl5BanBnXkFtZTcwMTA4ODM3OQ@@._V1_UY256_CR5,0,172,256_AL_.jpg"
-              alt=""
-            />
-            <div className="login-user__user-name">{globalState.test}</div>
-          </div>
-        </div>
-        <div className="login-user__buttons">
-          <button className="login-user__adult">Add Adult</button>
-          <button className="login-user__kid">Add Kid</button>
-        </div>
-      </div>
+      <Login />
     </div>
   );
 }
