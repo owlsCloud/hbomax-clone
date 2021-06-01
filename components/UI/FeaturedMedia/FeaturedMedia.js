18491ae3b0ea22a5f6f48e7da223fcb2597ef640
Gerardo Garza <owlscloud@gmail.com>
2021-06-01 17:21:30 -0500

    featured adjustment

--- components/UI/FeaturedMedia/FeaturedMedia.js
+++ components/UI/FeaturedMedia/FeaturedMedia.js
@@ -1,26 +1,31 @@
-const FeaturedMedia = (props) => {
+const FeaturedMedia = ({ videoUrl, title, location, mediaUrl }) => {
+  const clickedPlay = () => {
+    //SEND user to media page
+  };
   return (
     <div className="featured-media">
       <iframe
         className="featured-media__video"
         width="100%"
         height="100%"
-        src="https://www.youtube.com/embed/NYH2sLid0Zc?autoplay=1&mute=1&loop=1&start=16"
+        src={videoUrl}
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
         allowfullscreen
       />
       <div className="featured-media__bg">
         <div className="featured-media__container">
-          <div className="featured-media__title">Mortal Kombat</div>
-          <div className="featured-media__playing">NOW PLAYING</div>
-          <div className="featured-media__location">
-            In theaters and on HBO MAX. Streaming throughout May 23.
+          <div className="featured-media__title" onClick={clickedPlay}>
+            {title}
           </div>
+          <div className="featured-media__playing">NOW PLAYING</div>
+          <div className="featured-media__location">{location}</div>
           <div className="featured-media__buttons">
-            <div className="featured-media__play-btn">
+            <div className="featured-media__play-btn" onClick={clickedPlay}>
               <i className="fas fa-play" />
             </div>
-            <div className="featured-media__info-btn">MORE INFO</div>
+            <div className="featured-media__info-btn" onClick={clickedPlay}>
+              MORE INFO
+            </div>
           </div>
         </div>
       </div>
