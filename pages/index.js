d81931277502b59aa07c491728cea51124e20b4b
Gerardo Garza <owlscloud@gmail.com>
2021-06-01 15:40:31 -0500

    lazy loading

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import { useEffect } from "react";
 import { useRouter } from "next/router";
 import MainLayout from "../components/layouts/MainLayout";
 import FeaturedMedia from "../components/UI/FeaturedMedia/FeaturedMedia";
+import LazyLoad from "react-lazyload";
 
 import MediaRow from "../components/UI/MediaRow/MediaRow";
 import AuthCheck from "../components/AuthCheck";
@@ -13,36 +14,48 @@ export default function Home() {
   return AuthCheck(
     <MainLayout>
       <FeaturedMedia />
-      <MediaRow
-        title="Movies"
-        type="large-v"
-        endpoint="discover/movie?sort_by.desc=popularity&primary_release_year=2021"
-      />
-      <MediaRow
-        title="Series"
-        type="small-h"
-        endpoint="discover/tv?sort_by.desc=popularity&primary_release_year=2021"
-      />
-      <MediaRow
-        title="Action"
-        type="small-v"
-        endpoint="discover/movie?with_genres=28&primary_release_year=2021"
-      />
-      <MediaRow
-        title="Horror"
-        type="small-v"
-        endpoint="discover/movie?with_genres=27&primary_release_year=2021"
-      />
-      <MediaRow
-        title="Animation"
-        type="small-v"
-        endpoint="discover/movie?with_genres=16&primary_release_year=2021"
-      />
-      <MediaRow
-        title="Sci-Fi"
-        type="small-v"
-        endpoint="discover/movie?with_genres=878&primary_release_year=2021"
-      />
+      <LazyLoad height={680} placeholder={<h1>Laoding</h1>}>
+        <MediaRow
+          title="Movies"
+          type="large-v"
+          endpoint="discover/movie?sort_by.desc=popularity&primary_release_year=2021"
+        />
+      </LazyLoad>
+      <LazyLoad height={333}>
+        <MediaRow
+          title="Series"
+          type="small-h"
+          endpoint="discover/tv?sort_by.desc=popularity&primary_release_year=2021"
+        />
+      </LazyLoad>
+      <LazyLoad height={440}>
+        <MediaRow
+          title="Action"
+          type="small-v"
+          endpoint="discover/movie?with_genres=28&primary_release_year=2021"
+        />
+      </LazyLoad>
+      <LazyLoad>
+        <MediaRow
+          title="Horror"
+          type="small-v"
+          endpoint="discover/movie?with_genres=27&primary_release_year=2021"
+        />
+      </LazyLoad>
+      <LazyLoad height={200}>
+        <MediaRow
+          title="Animation"
+          type="small-v"
+          endpoint="discover/movie?with_genres=16&primary_release_year=2021"
+        />
+      </LazyLoad>
+      <LazyLoad>
+        <MediaRow
+          title="Sci-Fi"
+          type="small-v"
+          endpoint="discover/movie?with_genres=878&primary_release_year=2021"
+        />
+      </LazyLoad>
     </MainLayout>
   );
 }
