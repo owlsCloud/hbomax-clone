491e59d2ad724e0f1da6dd0bd9694944e628a4df
Gerardo Garza <owlscloud@gmail.com>
2021-06-01 12:22:57 -0500

    added skeleton for thumbnails

--- components/UI/MediaRow/MediaRow.js
+++ components/UI/MediaRow/MediaRow.js
@@ -1,4 +1,8 @@
+import { useState, useEffect } from "react";
+
 const MediaRow = (props) => {
+  const [loadingData, setLoadingData] = useState(true);
+
   const loopComp = (comp, digit) => {
     let thumbnails = [];
     for (let i = 1; i < digit; i++) {
@@ -6,22 +10,37 @@ const MediaRow = (props) => {
     }
     return thumbnails;
   };
+  const showThumbnails = () => {
+    setTimeout(() => setLoadingData(false), 3000);
+    return loadingData
+      ? loopComp(<Skeleton />, 10)
+      : loopComp(<Thumbnail />, 10);
+  };
+  //main component
   return (
     <div className={`media-row ${props.type}`}>
       <h3 className="media-row__title">{props.title}</h3>
-      <div className="media-row__thumbnails">
-        {loopComp(
-          <div className="media-row__thumbnail">
-            <img src="https://www.indiewire.com/wp-content/uploads/2019/12/nightingale-1.jpeg?w=675" />
-            <div className="media-row__top-layer">
-              <i className="fas fa-play" />
-            </div>
-          </div>,
-          10
-        )}
+      <div className="media-row__thumbnails">{showThumbnails()}</div>
+    </div>
+  );
+};
+
+const Thumbnail = () => {
+  return (
+    <div className="media-row__thumbnail">
+      <img src="https://www.indiewire.com/wp-content/uploads/2019/12/nightingale-1.jpeg?w=675" />
+      <div className="media-row__top-layer">
+        <i className="fas fa-play" />
       </div>
     </div>
   );
 };
 
+const Skeleton = () => {
+  return (
+    <div className="media-row__thumbnail-skeleton">
+      <div className="media-row__thumbnail-skeleton-img"></div>
+    </div>
+  );
+};
 export default MediaRow;
