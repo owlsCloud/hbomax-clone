3ef99c3396f1f3727fefafdfbb70c82c9d2183a9
Gerardo Garza <owlscloud@gmail.com>
2021-06-03 15:29:29 -0500

    changed side bar

--- components/UI/SideNav/SideNav.js
+++ components/UI/SideNav/SideNav.js
@@ -1,5 +1,5 @@
 import { useStateContext } from "../../HBOProvider";
-
+import Link from "next/link";
 const SideNav = (props) => {
   const globalState = useStateContext();
   return (
@@ -15,40 +15,20 @@ const SideNav = (props) => {
         <i className="fas fa-times" />
       </div>
       <ul className="side-nav__main">
-        <li>
-          <a href="/" className="active">
-            Home
-          </a>
+        <li onClick={() => globalState.setSideNavOpenAction(false)}>
+          <Link href="/">
+            <a>Home</a>
+          </Link>
         </li>
-        <li>
-          <a href="/">Movies</a>
+        <li onClick={() => globalState.setSideNavOpenAction(false)}>
+          <Link href="/movie">
+            <a>Movies</a>
+          </Link>
         </li>
-        <li>
-          <a href="/">Originals</a>
-        </li>
-        <li>
-          <a href="/">Just Added</a>
-        </li>
-        <li>
-          <a href="/">Last Chance</a>
-        </li>
-        <li>
-          <a href="/">Coming Soon</a>
-        </li>
-        <li>
-          <a href="/">Trending Now</a>
-        </li>
-      </ul>
-      <div className="side-nav__divider" />
-      <ul className="side-nav__main">
-        <li>
-          <a href="/">Music</a>
-        </li>
-        <li>
-          <a href="/">Series</a>
-        </li>
-        <li>
-          <a href="/">Movies</a>
+        <li onClick={() => globalState.setSideNavOpenAction(false)}>
+          <Link href="/tv">
+            <a>Series</a>
+          </Link>
         </li>
       </ul>
     </div>
