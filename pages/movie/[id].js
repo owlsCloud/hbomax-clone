832a7df8549d81cd016b106919da8e376db83d6d
Gerardo Garza <owlscloud@gmail.com>
2021-06-03 01:17:34 -0500

    making individual movie page

---
+++ pages/movie/[id].js
@@ -0,0 +1,40 @@
+import Head from "next/head";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { useRouter } from "next/router";
+import MainLayout from "../../components/layouts/MainLayout";
+import FeaturedMedia from "../../components/UI/FeaturedMedia/FeaturedMedia";
+import MediaRow from "../../components/UI/MediaRow/MediaRow";
+import CastInfo from "../../components/UI/CastInfo/CastInfo";
+import AuthCheck from "../../components/AuthCheck";
+export default function SingleMediaPage(props) {
+  const [mediaData, setMediaData] = useState(false);
+  const router = useRouter();
+  //const { id } = router.query;
+
+  useEffect(() => {
+    axios
+      .get(
+        `https://api.themoviedb.org/3/movie/${props.query.id}?api_key=e24d921b613656dd1dfa11782b7f23f3&language=en-US`
+      )
+      .then((res) => {
+        setMediaData(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+  return AuthCheck(
+    <MainLayout>
+      <FeaturedMedia title={mediaData.title} />
+      {/* <MediaRow title="More Like This" type="small-v" />*/}
+      <CastInfo />
+    </MainLayout>
+  );
+}
+
+export async function getServerSideProps(context) {
+  return {
+    props: { query: context.query },
+  };
+}
